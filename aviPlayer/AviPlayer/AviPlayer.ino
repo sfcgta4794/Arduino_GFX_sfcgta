79b5ead05414580a792b432108183ee29edf3c23
jimmycheungASTRI <jimmycheung@astri.org>
2026-04-15 17:49:48 +0800

    Created backup for AviPlayer.ino, Update AviPlayer.ino code with Gemini help, added new PIN config file

--- aviPlayer/AviPlayer/AviPlayer.ino
+++ aviPlayer/AviPlayer/AviPlayer.ino
@@ -26,236 +26,345 @@
 const char *root = "/root";
 const char *avi_folder = "/avi";
 
-#include <Wire.h>
-#include "es8311.h"
-// Dev Device Pins: <https://github.com/moononournation/Dev_Device_Pins.git>
-// #include "PINS_AD35-S3.h"
-// #include "PINS_ESP32-S3-Touch-LCD-1_3.h"
-// #include "PINS_ESP32-S3-Touch-LCD-1_3_prism.h"
-// #include "PINS_ESP32-S3-Touch-AMOLED-1_8.h"
-// #include "PINS_ESP32-S3-Touch-LCD-1_46.h"
-// #include "PINS_ESP32-S3-Touch-LCD-2_8.h"
-// #include "PINS_IBUBLY.h"
-// #include "PINS_JC1060P470.h"
-// #include "PINS_JC3248W535.h"
-// #include "PINS_JC8012P4A1.h"
-#include "PINS_T-DECK.h"
-// #include "PINS_T4_S3.h"
+// New code section
 
-#ifdef I2S_OUTPUT
-#define AVI_SUPPORT_AUDIO
-#endif
-
+#include <Arduino_GFX_Library.h>
+#include <LittleFS.h>
 #include "AviFunc.h"
 
-#ifdef AVI_SUPPORT_AUDIO
-#include "esp32_audio.h"
-#endif
+// --- Hardware Pins ---
+#define TFT_BL 21
+#define TFT_DC 9
+#define TFT_CS 13
+#define TFT_SCLK 12
+#define TFT_MOSI 11
+#define TFT_RST 10
 
-#include <string>
+// --- Screen Specs (ZJY147S0800TG01) ---
+#define TFT_WIDTH 172
+#define TFT_HEIGHT 320
+#define TFT_OFFSET_X 34
+#define GFX_SPEED 80000000UL
 
-#include <FFat.h>
-#include <LittleFS.h>
-#include <SPIFFS.h>
-#include <SD.h>
-#ifdef SOC_SDMMC_HOST_SUPPORTED
-#include <SD_MMC.h>
-#endif
+// Setup Display
+Arduino_DataBus *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCLK, TFT_MOSI, GFX_NOT_DEFINED);
+Arduino_ST7789 *gfx = new Arduino_ST7789(
+    bus, TFT_RST, 0 /* Rotation */, true /* IPS */, 
+    TFT_WIDTH, TFT_HEIGHT, 
+    TFT_OFFSET_X, 0, TFT_OFFSET_X, 0
+);
 
-void setup()
-{
-#ifdef DEV_DEVICE_INIT
-  DEV_DEVICE_INIT();
-#endif
-
+void setup() {
   Serial.begin(115200);
-  // Serial.setDebugOutput(true);
-  // while(!Serial);
-  Serial.println("AVI Player");
-
-  // If display and SD shared same interface, init SPI first
-#ifdef SPI_SCK
-  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-#endif
-
-  // Init Display
-  // if (!gfx->begin())
-  if (!gfx->begin(GFX_SPEED))
-  {
-    Serial.println("gfx->begin() failed!");
+  
+  // 1. Initialize Display
+  if (!gfx->begin(GFX_SPEED)) {
+    Serial.println("Display Init Failed!");
   }
-  gfx->fillScreen(RGB565_BLACK);
-#if defined(RGB_PANEL) || defined(DSI_PANEL) || defined(CANVAS)
-  gfx->flush(true /* force_flush */);
-#endif
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+  gfx->fillScreen(BLACK);
 
-#ifdef GFX_BL
-#if defined(ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR < 3)
-  ledcSetup(0, 1000, 8);
-  ledcAttachPin(GFX_BL, 0);
-  ledcWrite(0, 63);
-#else  // ESP_ARDUINO_VERSION_MAJOR >= 3
-  ledcAttachChannel(GFX_BL, 1000, 8, 1);
-  ledcWrite(GFX_BL, 63);
-#endif // ESP_ARDUINO_VERSION_MAJOR >= 3
-#endif // GFX_BL
-
-  // gfx->setTextColor(RGB565_WHITE, RGB565_BLACK);
-  // gfx->setTextBound(60, 60, 240, 240);
-
-#ifdef AVI_SUPPORT_AUDIO
-#ifdef AUDIO_EXTRA_PRE_INIT
-  AUDIO_EXTRA_PRE_INIT();
-#endif
-
-  i2s_init();
-
-#ifdef AUDIO_MUTE
-  pinMode(AUDIO_MUTE, OUTPUT);
-  digitalWrite(AUDIO_MUTE, LOW); // mute first
-#endif
-#endif // AVI_SUPPORT_AUDIO
-
-#if defined(SD_D1) && defined(SOC_SDMMC_HOST_SUPPORTED)
-#define FILESYSTEM SD_MMC
-  Serial.println("mount SD_MMC 4-bit");
-  SD_MMC.setPins(SD_SCK, SD_MOSI /* CMD */, SD_MISO /* D0 */, SD_D1, SD_D2, SD_CS /* D3 */);
-  if (!SD_MMC.begin(root, false /* mode1bit */, false /* format_if_mount_failed */, SDMMC_FREQ_HIGHSPEED))
-#elif defined(SD_SCK) && defined(SOC_SDMMC_HOST_SUPPORTED)
-#define FILESYSTEM SD_MMC
-  Serial.println("mount SD_MMC 1-bit");
-  pinMode(SD_CS, OUTPUT);
-  digitalWrite(SD_CS, HIGH);
-  SD_MMC.setPins(SD_SCK, SD_MOSI /* CMD */, SD_MISO /* D0 */);
-  if (!SD_MMC.begin(root, true /* mode1bit */, false /* format_if_mount_failed */, SDMMC_FREQ_HIGHSPEED))
-#elif defined(SD_CS)
-#define FILESYSTEM SD
-  Serial.println("mount SPI SD");
-  if (!SD.begin(SD_CS, SPI, 80000000, "/root"))
-#else
-#define FILESYSTEM FFat
-  Serial.println("mount FFat");
-  if (!FFat.begin(false, root))
-  // Serial.println("mount LittleFS");
-  // if (!LittleFS.begin(false, root))
-  // Serial.println("mount SPIFFS");
-  // if (!SPIFFS.begin(false, root))
-#endif
-  {
-    Serial.println("ERROR: File system mount failed!");
+  // 2. Initialize LittleFS
+  if (!LittleFS.begin()) {
+    Serial.println("LittleFS Mount Failed! Did you upload the data?");
+    return;
   }
-  else
-  {
-    output_buf_size = gfx->width() * gfx->height() * 2;
-#if defined(RGB_PANEL) | defined(DSI_PANEL)
-    output_buf = gfx->getFramebuffer();
-#else
-    output_buf = (uint16_t *)aligned_alloc(16, output_buf_size);
-#endif
-    if (!output_buf)
-    {
-      Serial.println("output_buf aligned_alloc failed!");
-    }
+  Serial.println("LittleFS Mounted Successfully.");
 
-    avi_init();
-  }
+  // 3. Initialize AVI decoder
+  avi_init();
 }
 
-void loop()
-{
-  Serial.printf("Open folder: %s\n", avi_folder);
-  File dir = FILESYSTEM.open(avi_folder);
-  if (!dir.isDirectory())
-  {
-    Serial.println("Not a directory");
-    delay(5000); // avoid error repeat too fast
+void loop() {
+  // Look for AVI files in the /avi folder of LittleFS
+  File root = LittleFS.open("/avi");
+  if (!root || !root.isDirectory()) {
+    Serial.println("Please create an /avi folder in your data directory.");
+    delay(5000);
+    return;
   }
-  else
-  {
-    File file = dir.openNextFile();
-    while (file)
-    {
-      if (!file.isDirectory())
-      {
-        std::string s = file.name();
-        // if ((!s.starts_with(".")) && (s.ends_with(".avi")))
-        if ((s.rfind(".", 0) != 0) && ((int)s.find(".avi", 0) > 0))
-        {
-          if (random(100) > 90)
-          {
-            s = root;
-            s += file.path();
-            if (avi_open((char *)s.c_str()))
-            {
-              Serial.println("AVI start");
-              gfx->fillScreen(RGB565_BLACK);
 
-#ifdef AVI_SUPPORT_AUDIO
-#ifdef AUDIO_MUTE
-              digitalWrite(AUDIO_MUTE, HIGH); // unmute
-#endif
-
-              if (avi_aRate > 0)
-              {
-                i2s_set_sample_rate(avi_aRate);
-              }
-
-              avi_feed_audio();
-
-              if (avi_aFormat == PCM_CODEC_CODE)
-              {
-                Serial.println("Start play PCM audio task");
-                BaseType_t ret_val = pcm_player_task_start();
-                if (ret_val != pdPASS)
-                {
-                  Serial.printf("pcm_player_task_start failed: %d\n", ret_val);
-                }
-              }
-              else if (avi_aFormat == MP3_CODEC_CODE)
-              {
-                Serial.println("Start play MP3 audio task");
-                BaseType_t ret_val = mp3_player_task_start();
-                if (ret_val != pdPASS)
-                {
-                  Serial.printf("mp3_player_task_start failed: %d\n", ret_val);
-                }
-              }
-              else
-              {
-                Serial.println("No audio task");
-              }
-#endif
-
-              avi_start_ms = millis();
-
-              Serial.println("Start play loop");
-              while (avi_curr_frame < avi_total_frames)
-              {
-#ifdef AVI_SUPPORT_AUDIO
-                avi_feed_audio();
-#endif
-
-                if (avi_decode())
-                {
-                  avi_draw(0, 0);
-                }
-              }
-
-#if defined(AVI_SUPPORT_AUDIO) && defined(AUDIO_MUTE)
-              digitalWrite(AUDIO_MUTE, LOW); // mute
-#endif
-
-              avi_close();
-              Serial.println("AVI end");
-
-              avi_show_stat();
-              delay(5000); // 5 seconds
-            }
+  File file = root.openNextFile();
+  while (file) {
+    String path = "/avi/" + String(file.name());
+    
+    if (path.endsWith(".avi")) {
+      Serial.printf("Playing: %s\n", path.c_str());
+      
+      if (avi_open((char*)path.c_str())) {
+        while (avi_curr_frame < avi_total_frames) {
+          if (avi_decode()) {
+            // Draw frame at 0,0
+            avi_draw(0, 0);
           }
+          yield(); // Keep ESP32 watchdog happy
         }
+        avi_close();
       }
-      file = dir.openNextFile();
     }
-    dir.close();
+    file = root.openNextFile();
   }
+  
+  Serial.println("Finished playing all videos. Restarting playlist...");
+  delay(1000);
 }
+
+
+
+
+// Old code section
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <Wire.h>
+// #include "es8311.h"
+// // Dev Device Pins: <https://github.com/moononournation/Dev_Device_Pins.git>
+// // #include "PINS_AD35-S3.h"
+// // #include "PINS_ESP32-S3-Touch-LCD-1_3.h"
+// // #include "PINS_ESP32-S3-Touch-LCD-1_3_prism.h"
+// // #include "PINS_ESP32-S3-Touch-AMOLED-1_8.h"
+// // #include "PINS_ESP32-S3-Touch-LCD-1_46.h"
+// // #include "PINS_ESP32-S3-Touch-LCD-2_8.h"
+// // #include "PINS_IBUBLY.h"
+// // #include "PINS_JC1060P470.h"
+// // #include "PINS_JC3248W535.h"
+// // #include "PINS_JC8012P4A1.h"
+// // #include "PINS_T-DECK.h" # uncomment this to revert to original version. 
+// // #include "PINS_T4_S3.h"
+
+// // Use customized layout
+// #include "PINS_ESP32_S3_LCD_1.47_CUSTOM.h"
+
+// #ifdef I2S_OUTPUT
+// #define AVI_SUPPORT_AUDIO
+// #endif
+
+// #include "AviFunc.h"
+
+// #ifdef AVI_SUPPORT_AUDIO
+// #include "esp32_audio.h"
+// #endif
+
+// #include <string>
+
+// #include <FFat.h>
+// #include <LittleFS.h>
+// #include <SPIFFS.h>
+// #include <SD.h>
+// #ifdef SOC_SDMMC_HOST_SUPPORTED
+// #include <SD_MMC.h>
+// #endif
+
+// void setup()
+// {
+// #ifdef DEV_DEVICE_INIT
+//   DEV_DEVICE_INIT();
+// #endif
+
+//   Serial.begin(115200);
+//   // Serial.setDebugOutput(true);
+//   // while(!Serial);
+//   Serial.println("AVI Player");
+
+//   // If display and SD shared same interface, init SPI first
+// #ifdef SPI_SCK
+//   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+// #endif
+
+//   // Init Display
+//   // if (!gfx->begin())
+//   if (!gfx->begin(GFX_SPEED))
+//   {
+//     Serial.println("gfx->begin() failed!");
+//   }
+//   gfx->fillScreen(RGB565_BLACK);
+// #if defined(RGB_PANEL) || defined(DSI_PANEL) || defined(CANVAS)
+//   gfx->flush(true /* force_flush */);
+// #endif
+
+// #ifdef GFX_BL
+// #if defined(ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR < 3)
+//   ledcSetup(0, 1000, 8);
+//   ledcAttachPin(GFX_BL, 0);
+//   ledcWrite(0, 63);
+// #else  // ESP_ARDUINO_VERSION_MAJOR >= 3
+//   ledcAttachChannel(GFX_BL, 1000, 8, 1);
+//   ledcWrite(GFX_BL, 63);
+// #endif // ESP_ARDUINO_VERSION_MAJOR >= 3
+// #endif // GFX_BL
+
+//   // gfx->setTextColor(RGB565_WHITE, RGB565_BLACK);
+//   // gfx->setTextBound(60, 60, 240, 240);
+
+// #ifdef AVI_SUPPORT_AUDIO
+// #ifdef AUDIO_EXTRA_PRE_INIT
+//   AUDIO_EXTRA_PRE_INIT();
+// #endif
+
+//   i2s_init();
+
+// #ifdef AUDIO_MUTE
+//   pinMode(AUDIO_MUTE, OUTPUT);
+//   digitalWrite(AUDIO_MUTE, LOW); // mute first
+// #endif
+// #endif // AVI_SUPPORT_AUDIO
+
+// #if defined(SD_D1) && defined(SOC_SDMMC_HOST_SUPPORTED)
+// #define FILESYSTEM SD_MMC
+//   Serial.println("mount SD_MMC 4-bit");
+//   SD_MMC.setPins(SD_SCK, SD_MOSI /* CMD */, SD_MISO /* D0 */, SD_D1, SD_D2, SD_CS /* D3 */);
+//   if (!SD_MMC.begin(root, false /* mode1bit */, false /* format_if_mount_failed */, SDMMC_FREQ_HIGHSPEED))
+// #elif defined(SD_SCK) && defined(SOC_SDMMC_HOST_SUPPORTED)
+// #define FILESYSTEM SD_MMC
+//   Serial.println("mount SD_MMC 1-bit");
+//   pinMode(SD_CS, OUTPUT);
+//   digitalWrite(SD_CS, HIGH);
+//   SD_MMC.setPins(SD_SCK, SD_MOSI /* CMD */, SD_MISO /* D0 */);
+//   if (!SD_MMC.begin(root, true /* mode1bit */, false /* format_if_mount_failed */, SDMMC_FREQ_HIGHSPEED))
+// #elif defined(SD_CS)
+// #define FILESYSTEM SD
+//   Serial.println("mount SPI SD");
+//   if (!SD.begin(SD_CS, SPI, 80000000, "/root"))
+// #else
+// #define FILESYSTEM FFat
+//   Serial.println("mount FFat");
+//   if (!FFat.begin(false, root))
+//   // Serial.println("mount LittleFS");
+//   // if (!LittleFS.begin(false, root))
+//   // Serial.println("mount SPIFFS");
+//   // if (!SPIFFS.begin(false, root))
+// #endif
+//   {
+//     Serial.println("ERROR: File system mount failed!");
+//   }
+//   else
+//   {
+//     output_buf_size = gfx->width() * gfx->height() * 2;
+// #if defined(RGB_PANEL) | defined(DSI_PANEL)
+//     output_buf = gfx->getFramebuffer();
+// #else
+//     output_buf = (uint16_t *)aligned_alloc(16, output_buf_size);
+// #endif
+//     if (!output_buf)
+//     {
+//       Serial.println("output_buf aligned_alloc failed!");
+//     }
+
+//     avi_init();
+//   }
+// }
+
+// void loop()
+// {
+//   Serial.printf("Open folder: %s\n", avi_folder);
+//   File dir = FILESYSTEM.open(avi_folder);
+//   if (!dir.isDirectory())
+//   {
+//     Serial.println("Not a directory");
+//     delay(5000); // avoid error repeat too fast
+//   }
+//   else
+//   {
+//     File file = dir.openNextFile();
+//     while (file)
+//     {
+//       if (!file.isDirectory())
+//       {
+//         std::string s = file.name();
+//         // if ((!s.starts_with(".")) && (s.ends_with(".avi")))
+//         if ((s.rfind(".", 0) != 0) && ((int)s.find(".avi", 0) > 0))
+//         {
+//           if (random(100) > 90)
+//           {
+//             s = root;
+//             s += file.path();
+//             if (avi_open((char *)s.c_str()))
+//             {
+//               Serial.println("AVI start");
+//               gfx->fillScreen(RGB565_BLACK);
+
+// #ifdef AVI_SUPPORT_AUDIO
+// #ifdef AUDIO_MUTE
+//               digitalWrite(AUDIO_MUTE, HIGH); // unmute
+// #endif
+
+//               if (avi_aRate > 0)
+//               {
+//                 i2s_set_sample_rate(avi_aRate);
+//               }
+
+//               avi_feed_audio();
+
+//               if (avi_aFormat == PCM_CODEC_CODE)
+//               {
+//                 Serial.println("Start play PCM audio task");
+//                 BaseType_t ret_val = pcm_player_task_start();
+//                 if (ret_val != pdPASS)
+//                 {
+//                   Serial.printf("pcm_player_task_start failed: %d\n", ret_val);
+//                 }
+//               }
+//               else if (avi_aFormat == MP3_CODEC_CODE)
+//               {
+//                 Serial.println("Start play MP3 audio task");
+//                 BaseType_t ret_val = mp3_player_task_start();
+//                 if (ret_val != pdPASS)
+//                 {
+//                   Serial.printf("mp3_player_task_start failed: %d\n", ret_val);
+//                 }
+//               }
+//               else
+//               {
+//                 Serial.println("No audio task");
+//               }
+// #endif
+
+//               avi_start_ms = millis();
+
+//               Serial.println("Start play loop");
+//               while (avi_curr_frame < avi_total_frames)
+//               {
+// #ifdef AVI_SUPPORT_AUDIO
+//                 avi_feed_audio();
+// #endif
+
+//                 if (avi_decode())
+//                 {
+//                   avi_draw(0, 0);
+//                 }
+//               }
+
+// #if defined(AVI_SUPPORT_AUDIO) && defined(AUDIO_MUTE)
+//               digitalWrite(AUDIO_MUTE, LOW); // mute
+// #endif
+
+//               avi_close();
+//               Serial.println("AVI end");
+
+//               avi_show_stat();
+//               delay(5000); // 5 seconds
+//             }
+//           }
+//         }
+//       }
+//       file = dir.openNextFile();
+//     }
+//     dir.close();
+//   }
+// }
---
+++ aviPlayer/AviPlayer/PINS_ESP32_S3_LCD_1.47_CUSTOM.h
@@ -0,0 +1,74 @@
+#pragma once
+
+// Check if selected right Board
+// #if !defined(ESP32) || !defined(CONFIG_IDF_TARGET_ESP32C6)
+// #error "Please select Board: ESP32C6"
+// #endif
+// // Check if enabled USB CDC On Boot
+// #if !ARDUINO_USB_CDC_ON_BOOT
+// #error "Please select USB CDC On Boot: Enabled"
+// #endif
+
+#define DEV_DEVICE_PINS
+
+/*
+ * 1. PIN DEFINITIONS
+ * Mapped according to your ESP32-S3-DEV_KIT-N16R8 setup.
+ */
+#define TFT_BL          21    // BLK
+#define TFT_CS          13    // CS
+#define TFT_DC          9     // DC
+#define TFT_RST         10    // RES
+#define TFT_MOSI        11    // SDA
+#define TFT_SCLK        12    // SCL
+#define TFT_MISO        -1    // Not connected
+
+/*
+ * 2. DISPLAY SPECIFICATIONS
+ * Specific to ZJY147S0800TG01 (1.47" 172x320)
+ */
+#define TFT_WIDTH       172
+#define TFT_HEIGHT      320
+#define TFT_OFFSET_X    34    // Required to center the 172px width in 240px memory
+#define TFT_OFFSET_Y    0
+#define GFX_SPEED       80000000UL // 80MHz SPI speed for the S3
+
+/*
+ * 3. GFX INSTANTIATION
+ * Using the Arduino_GFX library classes.
+ */
+#define GFX_SUPPORTED
+#include <Arduino_GFX_Library.h>
+
+// Initialize the ESP32-S3 SPI Bus
+Arduino_DataBus *bus = new Arduino_ESP32SPI(
+    TFT_DC, 
+    TFT_CS, 
+    TFT_SCLK, 
+    TFT_MOSI, 
+    TFT_MISO
+);
+
+// Initialize the ST7789 Driver
+Arduino_ST7789 *gfx = new Arduino_ST7789(
+    bus, 
+    TFT_RST, 
+    0,               /* rotation: 0 (portrait) */
+    true,            /* IPS: true for ZJY model */
+    TFT_WIDTH, 
+    TFT_HEIGHT,
+    TFT_OFFSET_X, TFT_OFFSET_Y, /* col/row offset 1 */
+    TFT_OFFSET_X, TFT_OFFSET_Y  /* col/row offset 2 */
+);
+
+#define GFX_BL          TFT_BL
+
+/*
+ * 4. OPTIONAL: SD CARD (Shared SPI Bus)
+ * If you add an SD card later for AVI playback, use these pins.
+ */
+// #define SD_SUPPORTED
+// #define SD_SCK          TFT_SCLK
+// #define SD_MOSI         TFT_MOSI
+// #define SD_MISO         14    // Example MISO pin for SD card
+// #define SD_CS           15    // Example CS pin for SD card
